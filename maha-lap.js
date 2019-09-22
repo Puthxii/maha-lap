f9ff39f8d2eede73f2045b9d6df50a7745eb8f1b
Puth1998 <Teenaputh12@hotmail.com>
2019-09-22 10:47:05 +0700

    refactor test

--- maha-lap.js
+++ maha-lap.js
@@ -1,33 +1,24 @@
 class MahaLap {
   static tamnaiWithDay(day) {
-    switch (true) {
+    switch (day) {
       case day === "จันทร์":
         return 2;
-        break;
       case day === "อังคาร":
         return 3;
-        break;
       case day === "พุธ":
         return 4;
-        break;
       case day === "พฤหัสบดี":
         return 5;
-        break;
       case day === "ศุกร์":
         return 6;
-        break;
       case day === "เสาร์":
         return 7;
-        break;
       case day === "สงกรานต์":
         return 13;
-        break;
       case day === "ฮาโลวีน":
         return 31;
-        break;
       case day === "คริสมาสต์":
         return 25;
-        break;
       default:
         return 1;
     }
