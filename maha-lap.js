5c9990e3b8014e740edd45099c0aedfa75148b9d
Puth1998 <Teenaputh12@hotmail.com>
2019-09-22 09:57:57 +0700

    add 3 testcase

---
+++ maha-lap.js
@@ -0,0 +1,16 @@
+class MahaLap {
+  static tamnaiWithDay(day) {
+    switch (true) {
+      case day === "จันทร์":
+        return 2;
+        break;
+      case day === "อังคาร":
+        return 3;
+        break;
+      default:
+        return 1;
+    }
+  }
+}
+
+module.exports = MahaLap;
---
+++ maha-lap.test.js
@@ -0,0 +1,17 @@
+const MahaLap = require("./maha-lap");
+describe("Test Maha-Lap", () => {
+  test("Test Tamnai wiht day", () => {
+    const result = MahaLap.tamnaiWithDay("จันทร์");
+    expect(result).toEqual(2);
+  });
+
+  test("Test Tamnai wiht day", () => {
+    const result = MahaLap.tamnaiWithDay("อังคาร");
+    expect(result).toEqual(3);
+  });
+
+  test("Test Tamnai wiht day", () => {
+    const result = MahaLap.tamnaiWithDay("อาทิตย์");
+    expect(result).toEqual(1);
+  });
+});
